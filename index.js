6661b612adb112c323ea770d7d18ed54b369a36c
jagtar <Jagtar.singh95@yahoo.in>
2023-06-07 02:38:27 -0400

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Import express, routes, env, mongoose
 const express = require("express");
 const routes = require("./routes");
+//enable cors
+var cors = require('cors')
 require('dotenv').config()
 var mongoose = require("mongoose");
 
@@ -10,6 +12,7 @@ var database = require("./database_config/database_tasklist");
 var bodyParser = require("body-parser"); // pull information from HTML POST (express4)
 var port = process.env.PORT;
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: "true" })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: "application/vnd.api+json" })); // parse application/vnd.api+json as json
